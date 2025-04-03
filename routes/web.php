ff05c75aa76a212e9f5b437ffaf18fefbe0d6831
Mykola Havrysh <decrier@ukr.net>
2025-04-03 12:23:47 +0200

    first

---
+++ routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use App\Http\Controllers\SongsController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::resource('/songs', SongsController::class);
