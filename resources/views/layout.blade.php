3260f6829a7dda06ae0ee3d87d51bb9134369330
Mykola Havrysh <decrier@ukr.net>
2025-04-03 13:55:35 +0200

    css

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,10 +5,37 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Songs</title>
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
 </head>
 <body>
+    <nav class="navbar navbar-expand-md bg-body-tertiary">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">REST-API</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="/songs">Songs</a>
+              </li>
+
+              <li class="nav-item">
+                <a class="nav-link" href="#">Labels</a>
+              </li>
+
+              <li class="nav-item">
+                <a class="nav-link" href="/songs/create">Neuen Song anlegen</a>
+              </li>
+
+          </div>
+        </div>
+      </nav>
     <div class="container">
         @yield('content')
     </div>
 </body>
+<script src="/js/bootstrap.bundle.min.js">
+
+</script>
 </html>
--- resources/views/songs/edit.blade.php
+++ resources/views/songs/edit.blade.php
@@ -3,31 +3,33 @@
 @section('content')
     <h2>Song bearbeiten</h2>
 
-    <form action="/songs/{{$song->id}}" method="post">
+    <form class="row" action="/songs/{{$song->id}}" method="post">
       @csrf
       @method('PUT')
 
-      <p>
-        <label for="title">Titel</label>
-        <input type="text" name="title" id="title" value="{{$song->title}}">
-      </p>
+      <div class="col-md-6">
+          <div class="mb-3">
+            <label for="title">Titel</label>
+            <input class="form-control" type="text" name="title" id="title" value="{{$song->title}}">
+          </div>
+          <div class="mb-3">
+            <label for="band">Band</label>
+            <input class="form-control" type="text" name="band" id="band" value="{{$song->band}}">
+          </div>
+      </div>
 
-      <p>
-        <label for="band">Band</label>
-        <input type="text" name="band" id="band" value="{{$song->band}}">
-      </p>
-
-      <p>
-        <label for="label">Label</label>
-        <select name="labels_id_ref" id="label">
-          @foreach ($labels as $label)
-              <option @if ($label->id === $song->labels_id_ref) selected @endif value="{{$label->id}}">{{$label->name}}</option>
-          @endforeach
-        </select>
-      </p>
-
-      <p>
-        <button type="submit">Änderung speichern</button>
-      </p>
+      <div class="col-md-6">
+          <div class="mb-3">
+            <label for="label">Label</label>
+            <select class="form-select" name="labels_id_ref" id="label">
+              @foreach ($labels as $label)
+                  <option @if ($label->id === $song->labels_id_ref) selected @endif value="{{$label->id}}">{{$label->name}}</option>
+              @endforeach
+            </select>
+          </div>
+          <div class="mt-3">
+            <button class="btn btn-success" type="submit">Änderung speichern</button>
+          </div>
+      </div>
     </form>
 @endsection
